fefc397a4416b483bf6709402aead68acbdadd15
Rats <juliocesarmmc@hotmail.com>
2019-08-05 15:33:01 -0300

    backup, implementações do PSA

--- PSA/simulated.cpp
+++ PSA/simulated.cpp
@@ -6,69 +6,217 @@ void pareto_simulated_anneling()
 {
     /* Temperatura atual do PSA */
     double psa_current_temperature = psa_starting_temperature;
-
     /* Preenche nuvem com "length_cloud" soluções */
     starting_cloud();
 
-    while(psa_current_temperature >= psa_final_temperature)
+    ofstream init;
+    init.open("result_inicial.txt");
+    for (Solution e: cloud_solutions)
+      init << e.wcrt << '\t' << e.time_mean_burst << '\n';
+    init.close();
+
+    /* Lista auxiliar para criar a nova nuvem de soluções*/
+    list<Solution> list_aux;
+
+    while(psa_current_temperature > psa_final_temperature)
     {
+        printf("Temperatura %.2f\n", psa_current_temperature);
+        list_aux.clear();
+
         /* Percorre a nuvem de soluções e gera vizinhos para cada Solução */
         for (list<Solution>::iterator it = cloud_solutions.begin(); it != cloud_solutions.end(); it++)
         {
             /* Gera Vizinho */
             psa_solution_neighbor = neighborhood_search(*it);
-
             /* Gera resultados na simulação */
             run_simulation(psa_solution_neighbor);
+            /* Verifica se é uma solução dominate */
+            if (is_dominat(psa_solution_neighbor, *it))
+            {
+                /* Se for dominate atualiza nas atuais e coloca no conjunto de solução fronteiras */
+                list_aux.push_back(psa_solution_neighbor);
+                add_cloud_frontier(psa_solution_neighbor);
+            }else{
+                /* Caso não, ajusta escalas */
+                adjusts_scale(psa_solution_neighbor, *it);
+                /* Faça a probabilidade, para poder ser aceita como atual (Não adiciona nas fronteiras)*/
+                if (accept(*it, psa_solution_neighbor, psa_current_temperature))
+                {
+                    list_aux.push_back(psa_solution_neighbor);
+                    std::cout << "sim" << '\n';
+                }
+                else
+                {
+                    list_aux.push_back(*it);
+                }
+            }
         }
+        /* Atualiza a nova nuvem de soluções*/
+        cloud_solutions = list_aux;
         /* Decaimento da Temperatura*/
         psa_current_temperature *= psa_alpha_temperature;
     }
+    ofstream out;
+    out.open("result_final.txt");
+    for (Solution e: cloud_frontier)
+      out << e.wcrt << '\t' << e.time_mean_burst << '\n';
+    out.close();
+    out.open("result_fronteira.txt");
+    for (Solution e: get_best_frontier())
+      out << e.wcrt << '\t' << e.time_mean_burst << '\n';
+    out.close();
+}
+
+list<Solution> get_best_frontier()
+{
+  list<Solution> dominats;
+  bool dominat = true;
+  for (list<Solution>::iterator it = cloud_frontier.begin(); it != cloud_frontier.end(); it++)
+  {
+    for (list<Solution>::iterator it2 = cloud_frontier.begin(); it2 != cloud_frontier.end(); it2++)
+        if (is_dominat(*it2,*it))
+        {
+            dominat = false;
+            break;
+        }
+
+    if (dominat)
+        dominats.push_front(*it);
+
+    dominat = true;
+  }
+    return dominats;
+}
+
+void add_cloud_frontier(Solution new_s)
+{
+    cloud_frontier.push_front(new_s);
+    if (cloud_frontier.size() > length_frontier)
+    {
+      pair<list<Solution>::iterator, unsigned int>* dominats = new pair<list<Solution>::iterator, unsigned int>[cloud_frontier.size()];
+      int pos           = 0;
+      unsigned int cont = 0;
+      for (list<Solution>::iterator it = cloud_frontier.begin(); it != cloud_frontier.end(); it++)
+      {
+        cont = 0;
+        for (list<Solution>::iterator it2 = cloud_frontier.begin(); it2 != cloud_frontier.end(); it2++)
+          if (is_dominat(*it2, *it))
+             cont++;
+        dominats[pos++] = make_pair(it, cont);
+      }
+      pos  = 0;
+      cont = 0;
+      for (int i=0; i < cloud_frontier.size(); i++)
+          if (dominats[i].second >= cont)
+          {
+              cont = dominats[i].second;
+              pos  = i;
+          }
+      cloud_frontier.erase(dominats[pos].first);
+    }
+}
+
+void adjusts_scale(Solution& new_s, Solution base_s)
+{
+    if (new_s.wcrt <= base_s.wcrt)
+        new_s.scale_wcrt *= psa_alpha_scale;
+    else
+        new_s.scale_wcrt /= psa_alpha_scale;
+
+    if (new_s.frames_burst <= base_s.frames_burst)
+        new_s.scale_frames_burst *= psa_alpha_scale;
+    else
+        new_s.scale_frames_burst /= psa_alpha_scale;
+
+    if (new_s.time_mean_burst <= base_s.time_mean_burst)
+        new_s.scale_time_mean_burst *= psa_alpha_scale;
+    else
+        new_s.scale_time_mean_burst /= psa_alpha_scale;
+
+    double norm = new_s.scale_wcrt;
+    norm       += new_s.scale_frames_burst;
+    norm       += new_s.scale_time_mean_burst;
+
+    new_s.scale_wcrt            /= norm;
+    new_s.scale_frames_burst    /= norm;
+    new_s.scale_time_mean_burst /= norm;
+}
+
+bool accept(Solution s1, Solution s2, double temp)
+{
+    double maximo = max((s2.scale_wcrt*(s1.wcrt-s2.wcrt)/temp),(s2.scale_time_mean_burst*(s1.time_mean_burst-s2.time_mean_burst)/temp));
+    // minimo = min<double>(minimo, (s2.scale_time_mean_burst*(s1.time_mean_burst-s2.time_mean_burst)/temp));
+    double sort = ((double)rand()/RAND_MAX);
+    // std::cout << sort << " = contra = " << min<double>(1,exp(maximo)) << '\n';
+    return (min<double>(1,exp(maximo)) <= sort);
+}
+
+bool is_dominat(Solution s1, Solution s2)
+{
+    return (((s1.wcrt <= s2.wcrt) && (s1.time_mean_burst <= s2.time_mean_burst)) &&
+            ((s1.wcrt <  s2.wcrt) || (s1.time_mean_burst <  s2.time_mean_burst)) );
+}
+
+void random_scale(Solution& solucao)
+{
+    double r1 = (double)((rand()%98)+1);
+    double r2 = (double)(((rand()%(99-(int)(r1)))+1));
+    double r3 = 100-(r1+r2);
+
+    solucao.scale_wcrt            = (double) (r1);
+    solucao.scale_frames_burst    = (double) (r2);
+    solucao.scale_time_mean_burst = (double) (r3);
+}
+
+void starting_cloud()
+{
+    // FIRST !!
+    random_scale(psa_solution_first);
+    run_simulation(psa_solution_first);
+    cloud_solutions.push_back(psa_solution_first);
+
+    for (unsigned int i=1; i < length_cloud; i++)
+    {
+        cloud_solutions.push_back(random_neighbor(psa_solution_first));
+    }
+}
+
+void run_simulation(Solution& solucao)
+{
+    simulator = new Simulator_CAN(solucao.candb_solution, length_frames);
+    simulator->run_simulation(TIME_SIMULATION);
+    solucao.wcrt            =  simulator->wcrt;
+    // solucao.wcrt            =  rand()%1000;
+    solucao.frames_burst    =  simulator->frames_burst;
+    // solucao.frames_burst    =  rand()%100000;
+    solucao.time_mean_burst =  simulator->time_mean_burst;
+    // solucao.time_mean_burst =  rand()%1000;
+    delete(simulator);
 }
 
 Solution random_neighbor(Solution base)
 {
     Solution random_solution = base;
-
-    unsigned int r1 = ((rand()%98)+1);
-    unsigned int r2 = (((rand()%(99-r1))+1));
-    unsigned int r3 = 100 - (r1+r2);
-
-    random_solution.scale_wcrt            = (double) (r1/100);
-    random_solution.scale_frames_burst    = (double) (r2/100);
-    random_solution.scale_time_mean_burst = (double) (r3/100);
+    random_solution.candb_solution = new Frame_CAN[length_frames];
 
     for (unsigned int i=0; i < length_frames; i++)
     {
+        random_solution.candb_solution[i] = base.candb_solution[i];
         random_solution.candb_solution[i].delay_time = (double)(rand()%((int) random_solution.candb_solution[i].deadline_time));
     }
     run_simulation(random_solution);
+    random_scale(random_solution);
     return random_solution;
-}
-
-void starting_cloud()
-{
-    cloud_solutions.push_back(psa_solution_current);
-    for (unsigned int i=0; i < length_cloud-1; i++)
-    {
-        cloud_solutions.push_back(random_neighbor(psa_solution_current));
-    }
-}
-
-void run_simulation(Solution &solucao)
-{
-    main_simulated(psa_arq_times_simulation, TIME_SIMULATION);
-    solucao.wcrt            =  wcrt;
-    solucao.frames_burst    =  max_length_queue;
-    solucao.time_mean_burst =  time_mean_queue;
 }
 
 Solution neighborhood_search(Solution solucao)
 {
     Solution new_solution = solucao;
+    new_solution.candb_solution = new Frame_CAN[length_frames];
 
     for (unsigned int i=0; i < length_frames; i++)
     {
+        new_solution.candb_solution[i] = solucao.candb_solution[i];
         new_solution.candb_solution[i].delay_time += (double)((rand()%11)-5);
         if (new_solution.candb_solution[i].delay_time < 0)
             new_solution.candb_solution[i].delay_time = 0;
@@ -83,6 +231,41 @@ Solution neighborhood_search(Solution solucao)
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
+    FILE* arq  = fopen(argv[1], "r");
+    Frame_CAN* frames = get_CANDB(arq, length_frames);
+
+    // for (size_t i = 0; i < length; i++) {
+    //     printf("%u\t%lf\t%lf\t%lf\t%lf\n", frames[i].id, frames[i].cycle_time,
+    //             frames[i].deadline_time, frames[i].delay_time);
+    // }
+
+    psa_solution_first.candb_solution = frames;
+    psa_starting_temperature = 10000;
+    psa_final_temperature     = 10;
+    psa_alpha_temperature    = 0.98;
+    psa_alpha_scale          = 0.1;
+    length_cloud             = 100;
+    length_frontier          = 10;
+    pareto_simulated_anneling();
+
+    // Solution a,b;
+    // a.scale_wcrt = 1;
+    // a.wcrt = 10;
+    // a.scale_time_mean_burst = 1;
+    // a.time_mean_burst = 10;
+    // b.scale_wcrt = 10;
+    // b.wcrt = 0;
+    // b.scale_time_mean_burst = 10;
+    // b.time_mean_burst = 0;
+    //
+    // std::cout << accept(b,a,800) << '\n';
+
+    // Simulator_CAN sim(frames, length);
+    // sim.run_simulation(100);
+    //
+    // std::cout << "WCRT > " << sim.wcrt << "\nTIME_BURST > " << sim.time_mean_burst
+    //           << "\nFrames Burst > " << sim.frames_burst
+    //           << "\nDeadLines > " << sim.deadlines << '\n';
 
     return 0;
 }
--- PSA/simulated.hpp
+++ PSA/simulated.hpp
@@ -4,13 +4,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <list>
-#include "./../Simulador deterministico/simulador.h"
+#include <fstream>
+#include "./../Simulador deterministico/simulador.hpp"
 
-#define       LENGTH_CHAR           64
-#define       TIME_SIMULATION       120000
-
-class Solution;
-class Frame_CAN;
+#define     LENGTH_CHAR           64
+#define     TIME_SIMULATION       12000
 
 /******************************************************************
 
@@ -28,23 +26,8 @@ class Solution
       double       time_mean_burst;       //  Tempo medio dos bursts
       double       scale_time_mean_burst; //  Escalar para função Objetiva
       Frame_CAN*   candb_solution;        //  Conjunto de frames
-
 };
 
-/******************************************************************
-
-Estrutura (classe) para representar um frame CAN
-
-*******************************************************************/
-
-class Frame_CAN
-{
-  public:
-      unsigned int id:11;               // ID da mensagem
-      double       cycle_time;          // Periodo de ciclo
-      double       deadline_time;       // Tempo de limite maximo em fila
-      double       delay_time;          // Tempo proposto para start delay (offset)
-};
 
 /******************************************************************
 
@@ -55,26 +38,36 @@ Variaveis de controle do simulated anneling
 double    psa_starting_temperature;     // Temperatura inicial do PSA
 double    psa_final_temperature;         // Temperatura final do PSA
 double    psa_alpha_temperature;        // Escalar multiplicativo para decaimento da Temperatura
+double    psa_alpha_scale;              // Escalar multiplicativo para função objetiva
 
 Solution  psa_solution_neighbor;        // Solução Vizinho da atual
-Solution  psa_solution_current;         // Solução atual do PSA
+Solution  psa_solution_first;            // Solução atual do PSA
 
 unsigned int        length_frames;      // Numero de frames na solução
 unsigned int        length_cloud;       // Numero de soluções na nuvem de soluções
+unsigned int        length_frontier;    // Numero de soluções na nuvem de soluções
 std::list<Solution> cloud_solutions;    // Conjunto com todas as soluções atuais
+std::list<Solution> cloud_frontier;     // Conjunto com todas as soluções "melhores"
 
-char      psa_arq_times_simulation[LENGTH_CHAR] = ".tempos.txt";
-char      psa_arq_result[LENGTH_CHAR];
+Simulator_CAN* simulator;
+
+char            psa_arq_result[LENGTH_CHAR];
 /******************************************************************
 
 Escopos das funções implementadas
 
 *******************************************************************/
 
-void     pareto_simulated_anneling();
 void     starting_cloud();
+void     pareto_simulated_anneling();
+void     random_scale(Solution& solucao);
 void     run_simulation(Solution& solucao);
-Solution neighborhood_search(Solution solucao);
+void     add_cloud_frontier(Solution new_s);
+void     adjusts_scale(Solution& new_s, Solution base_s);
+bool     is_dominat(Solution s1, Solution s2);
+bool     accept(Solution s1, Solution s2, double temp);
 Solution random_neighbor(Solution base);
+Solution neighborhood_search(Solution solucao);
+std::list<Solution> get_best_frontier();
 
 #endif
